d5c5a806cde3897fd7a629507327f7be6fd406ce
BijianWu <384318185@qq.com>
2023-08-19 00:14:31 +0100

    added react ui packages

--- src/App.js
+++ src/App.js
@@ -1,8 +1,10 @@
+import { Button } from "@mui/material";
 import React, { useState } from "react";
 
 export default function App(){
 
     return <>
         <div>Hello world</div>
+        <Button variant="contained">Hello world</Button>
     </>
 }
