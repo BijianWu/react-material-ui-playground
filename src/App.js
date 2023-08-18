a4dfd0b6aba8710c2cffc9c9d8ad5a51ecbe1bd4
BijianWu <384318185@qq.com>
2023-08-19 00:46:08 +0100

    add in ResponsiveAppBar

--- src/App.js
+++ src/App.js
@@ -2,11 +2,12 @@ import { Button } from "@mui/material";
 import React, { useState } from "react";
 import Nav from "./components/Nav.js";
 import SelectElement from "./components/SelectElement.js";
+import ResponsiveAppBar from "./components/ResponsiveAppBar.js";
 
 export default function App(){
 
     return <>
-         <Nav/>     
+         <ResponsiveAppBar/>     
          <SelectElement />
         <div>Hello world</div>
         <Button variant="contained">Hello world</Button>
