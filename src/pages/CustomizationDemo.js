84fcfc8a2480913100564f18910158d13f7745c4
BijianWu <384318185@qq.com>
2023-08-21 23:20:58 +0100

    select thumb component inside the Slider component to apply the customization to change the thumb from circle to square

--- src/pages/CustomizationDemo.js
+++ src/pages/CustomizationDemo.js
@@ -9,6 +9,9 @@ export default function CustomizationDemo() {
                 sx={{
                     width: 800,
                     color: 'success.main', // secondary.main
+                    '& .MuiSlider-thumb': { // [hash]-Mui[Component name]-[name of the slot] select the thumb slot inside the Slider component
+                        borderRadius: '1px',
+                    },
                 }}
             />
         </>
