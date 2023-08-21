db088f5fc4a3f756bf189a69934c8b5b6b5d6226
BijianWu <384318185@qq.com>
2023-08-21 23:49:16 +0100

    use styled as the props to style the component

--- src/pages/CustomizationDemo.js
+++ src/pages/CustomizationDemo.js
@@ -1,6 +1,20 @@
 import { Button, Slider } from '@mui/material';
 import Typography from '@mui/material/Typography';
 import "../assets/css/styles.css";
+import { alpha, styled } from '@mui/material/styles';
+
+const SuccessSlider = styled(Slider)(({ theme }) => ({
+    width: 300,
+    color: theme.palette.success.main,
+    '& .MuiSlider-thumb': {
+      '&:hover, &.Mui-focusVisible': {
+        boxShadow: `0px 0px 0px 8px ${alpha(theme.palette.success.main, 0.16)}`,
+      },
+      '&.Mui-active': {
+        boxShadow: `0px 0px 0px 14px ${alpha(theme.palette.success.main, 0.16)}`,
+      },
+    },
+  }));
 
 export default function CustomizationDemo() {
     return (
@@ -20,6 +34,8 @@ export default function CustomizationDemo() {
             <div className="custom">
             <Button disabled className="custom Button">MYBTN</Button>
             </div>
+
+            <SuccessSlider defaultValue={30} />
 
         </>
     )
