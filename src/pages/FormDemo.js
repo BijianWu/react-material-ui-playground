c6e47dfc8d9ca98f32139c82c3a6f3b5a94d59d0
BijianWu <384318185@qq.com>
2023-08-22 00:25:50 +0100

    add in text fields into form demo js

--- src/pages/FormDemo.js
+++ src/pages/FormDemo.js
@@ -2,15 +2,25 @@ import * as React from 'react';
 import { styled, createTheme, ThemeProvider } from '@mui/material/styles';
 import { deepPurple } from '@mui/material/colors';
 import Avatar from '@mui/material/Avatar';
-import { Typography } from '@mui/material';
+import { Divider, FormControl, FormHelperText, Input, InputLabel, TextField, Typography } from '@mui/material';
 
 export default function FormDemo() {
 
     return (
         <>
-            <Typography>
-                form demo is here
-            </Typography>
+        
+            <form>
+                <FormControl sx={{m:3}}>
+                    <InputLabel htmlFor="my-input">Email address</InputLabel>
+                    <Input  aria-describedby="my-helper-text" />
+                    <FormHelperText >We'll never share your email.</FormHelperText>
+                </FormControl>
+
+                <FormControl sx={{m:3}}>
+                    <TextField label="Name" variant="outlined" />
+                </FormControl>
+
+            </form>
         </>
     )
 }
