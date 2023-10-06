e56ea09c1d183d02e694979758fca6060da8b66a
BijianWu <384318185@qq.com>
2023-10-06 01:13:59 +0100

    show form error pop up on submit

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,6 +1,7 @@
 import { Button, Paper, TextField } from '@mui/material';
 import Typography from '@mui/material/Typography';
 import { useFormik } from 'formik';
+import { useState } from 'react';
 import * as Yup from "yup";
 
 
@@ -15,6 +16,7 @@ export default function Login() {
             console.log("onSumbit", values);
         },
         validate: (values) => {
+            console.log("validate", values);
             const errors = {};
             if(!values.email){
                 errors.email = "Email is required";
@@ -24,13 +26,21 @@ export default function Login() {
                 errors.password = "Password is required";
             }
 
+
             return errors;
         }
     });
 
+    const customHandleSubmit = (e) => {
+        formik.handleSubmit(e);
+
+        if(formik.submitCount ==0 || Object.keys(formik.errors).length > 0){
+            console.log("form error, please check ")
+        }
+    }
     return (
         <Paper sx={{mt: "30px"}}>
-        <form onSubmit={formik.handleSubmit}>
+        <form onSubmit={customHandleSubmit}>
             <TextField id="outlined-basic" label="Email" name="email" variant="outlined" value={formik.values.email} onChange={formik.handleChange} onBlur={formik.handleBlur} />
             {formik.errors.email && formik.touched.email && <p style={{color: "red"}}>{formik.errors.email}</p>}
             <TextField id="outlined-basic" label="Password" name="password" variant="outlined" value={formik.values.password} onChange={formik.handleChange} onBlur={formik.handleBlur} />
