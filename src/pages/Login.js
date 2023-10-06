238715483aaf751807d98888ddb2c306f0b28452
BijianWu <384318185@qq.com>
2023-10-06 01:23:14 +0100

    zoned time lib

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -3,9 +3,22 @@ import Typography from '@mui/material/Typography';
 import { useFormik } from 'formik';
 import { useState } from 'react';
 import * as Yup from "yup";
+import { compareAsc, format, formatDistance, subDays } from 'date-fns'
 
 
 export default function Login() {
+
+    // code play ground
+    const { zonedTimeToUtc, utcToZonedTime, format } = require('date-fns-tz')
+    const utcDate = zonedTimeToUtc('2023-10-05 18:01:36.386', 'Europe/London')
+    console.log(utcDate)
+
+
+   const value = formatDistance(subDays(new Date(), 3), new Date(), { addSuffix: true })
+    console.log(value)
+    // code play ground
+
+
 
     const formik = useFormik({
         initialValues: {
